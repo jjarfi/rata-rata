f592232670a8b614b01fd4e5199dbdcb5e31a78c
jjarfi <pacelinux@gmail.com>
2018-09-21 14:44:34 -0400

    Auto Spinner Bot

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: pacevil
+ *
+ * Created on September 21, 2018, 2:42 PM
+ */
+
+#include <iostream>
+#include <ostream>
+#include <ios>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+int a, b, c;
+a = 4;
+b = 7;
+c = (a > b)? 8 : 100;
+cout<<"Nilai c adalah : "<< c << endl;
+return 0;
+}
